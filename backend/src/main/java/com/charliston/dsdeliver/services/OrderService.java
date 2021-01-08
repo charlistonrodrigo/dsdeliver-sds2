416112d911acc62d06afe878ec2544624d02fbb2
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-07 21:00:32 -0300

    GET/ orders

---
+++ backend/src/main/java/com/charliston/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.charliston.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.charliston.dsdeliver.dto.OrderDTO;
+import com.charliston.dsdeliver.entities.Order;
+import com.charliston.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List <Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
