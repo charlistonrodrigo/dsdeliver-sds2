fc4b289f96a4e7e41e5b0a55d32eac365ee449ae
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-11 00:20:55 -0300

    Search order location

---
+++ front-web/src/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8085';
+const mapboxToken = "pk.eyJ1IjoiY2hhcmxpc3RvbnJvZHJpZ28iLCJhIjoiY2tqcnA4cXBuMDIwaTJ5bjNpdWZiZXhoMyJ9.8AnAJ7GHhuUBu_PsklUexQ"
+
+export function fetchProducts() {
+    return axios(`${API_URL}/products`)   
+}
+
+export function fetchLocalMapBox(local: string) {
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
