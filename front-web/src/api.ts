978f0b7073bc21a9833124c5361d43fb278a841f
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-11 23:01:48 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 const API_URL = 'http://localhost:8085';
 const mapboxToken = "pk.eyJ1IjoiY2hhcmxpc3RvbnJvZHJpZ28iLCJhIjoiY2tqcnA4cXBuMDIwaTJ5bjNpdWZiZXhoMyJ9.8AnAJ7GHhuUBu_PsklUexQ"
@@ -9,4 +10,8 @@ export function fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload:OrderPayload) {
+    return axios.post(`${API_URL}/orders`, payload)
 }
