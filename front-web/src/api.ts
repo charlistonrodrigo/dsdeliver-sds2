e33d30379668d0a60c018bf6e6d3428218f4bce1
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-17 11:14:18 -0300

    Fix total

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
+//const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 const mapboxToken = "pk.eyJ1IjoiY2hhcmxpc3RvbnJvZHJpZ28iLCJhIjoiY2tqcnA4cXBuMDIwaTJ5bjNpdWZiZXhoMyJ9.8AnAJ7GHhuUBu_PsklUexQ"
 
 export function fetchProducts() {
